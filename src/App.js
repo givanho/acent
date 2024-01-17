c113ac0fd5132830d98a56d32014662e6c12af9c
Given Fyneface <fynefaceg@gmail.com>
2024-01-17 10:15:25 +0100

    second

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
                 </label>
                 <div className="text-sm">
                   <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                    Forgot password?
+                    Forgoten password?
                   </a>
                 </div>
               </div>
