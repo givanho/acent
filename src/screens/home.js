e0b5f3fa8c3c3d22f502684605321e60a5d10439
Given Fyneface <fynefaceg@gmail.com>
2024-01-18 14:06:23 +0100

    bb

--- src/screens/home.js
+++ src/screens/home.js
@@ -20,114 +20,7 @@ const Home = () => {
     <div>
  
  
-     {/* <Carousel className="rounded-none" autoplay="true" autoplayDelay = "5000" loop="true" transition={{ duration: 1 }}  >
-       <div className="relative h-full w-full">
-       
-       <img
-        src={firstSlide} alt="office"
-        className="h-full w-full object-cover " 
-      />
-         <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
-           <div className="w-3/4 text-center md:w-2/4">
-             <Typography
-               variant="h1"
-               color="white"
-               className="mb-4 text-3xl md:text-4xl lg:text-5xl"
-             >
-               The Beauty of Nature
-             </Typography>
-             <Typography
-               variant="lead"
-               color="white"
-               className="mb-12 opacity-80"
-             >
-               It is not so much for its beauty that the forest makes a claim
-               upon men&apos;s hearts, as for that subtle something, that quality
-               of air that emanation from old trees, that so wonderfully changes
-               and renews a weary spirit.
-             </Typography>
-             <div className="flex justify-center gap-2">
-               <Button size="lg" color="white">
-                 Explore
-               </Button>
-               <Button size="lg" color="white" variant="text">
-                 Gallery
-               </Button>
-             </div>
-           </div>
-         </div>
-       </div>
-       <div className="relative h-full w-full">
-       <img
-        src={secondSlide} alt="slide2"
-        className="h-full w-full object-cover"
-      />
-         <div className="absolute inset-0 grid h-full w-full items-center bg-black/75">
-           <div className="w-3/4 pl-12 md:w-2/4 md:pl-20 lg:pl-32">
-             <Typography
-               variant="h1"
-               color="white"
-               className="mb-4 text-3xl md:text-4xl lg:text-5xl"
-             >
-               The Beauty of Nature
-             </Typography>
-             <Typography
-               variant="lead"
-               color="white"
-               className="mb-12 opacity-80"
-             >
-               It is not so much for its beauty that the forest makes a claim
-               upon men&apos;s hearts, as for that subtle something, that quality
-               of air that emanation from old trees, that so wonderfully changes
-               and renews a weary spirit.
-             </Typography>
-             <div className="flex gap-2">
-               <Button size="lg" color="white">
-                 Explore
-               </Button>
-               <Button size="lg" color="white" variant="text">
-                 Gallery
-               </Button>
-             </div>
-           </div>
-         </div>
-       </div>
-       <div className="relative h-full w-full">
-       <img
-        src={thirdSlide} alt="slide3"
-        className="h-full w-full object-cover"
-      />
-         <div className="absolute inset-0 grid h-full w-full items-end bg-black/75">
-           <div className="w-3/4 pl-12 pb-12 md:w-2/4 md:pl-20 md:pb-20 lg:pl-32 lg:pb-32">
-             <Typography
-               variant="h1"
-               color="white"
-               className="mb-4 text-3xl md:text-4xl lg:text-5xl"
-             >
-               The Beauty of Nature
-             </Typography>
-             <Typography
-               variant="lead"
-               color="white"
-               className="mb-12 opacity-80"
-             >
-               It is not so much for its beauty that the forest makes a claim
-               upon men&apos;s hearts, as for that subtle something, that quality
-               of air that emanation from old trees, that so wonderfully changes
-               and renews a weary spirit.
-             </Typography>
-             <div className="flex gap-2">
-               <Button size="lg" color="white">
-                 Explore
-               </Button>
-               <Button size="lg" color="white" variant="text">
-                 Gallery
-               </Button>
-             </div>
-           </div>
-         </div>
-       </div>
-     </Carousel> */}
+     
      <Carousel className="rounded-none" autoplay="true" autoplayDelay="5000" loop="true" transition={{ duration: 1 }}>
   <div className="relative h-full w-full">
     <div className="h-full w-full overlay" >
@@ -163,7 +56,80 @@ const Home = () => {
       </div>
     </div>
   </div>
-
+  <div className="relative h-full w-full">
+  <div className="h-full w-full overlay" >
+       <img
+        src={secondSlide} alt="slide2"
+        className="h-full w-full object-cover"
+      />
+      </div>
+         <div className="absolute inset-0 grid h-full w-full items-center bg-black/75">
+           <div className="w-3/4 pl-12 md:w-2/4 md:pl-20 lg:pl-32">
+             <Typography
+               variant="h1"
+               color="white"
+               className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+             >
+               The Beauty of Nature
+             </Typography>
+             <Typography
+               variant="lead"
+               color="white"
+               className="mb-12 opacity-80"
+             >
+               It is not so much for its beauty that the forest makes a claim
+               upon men&apos;s hearts, as for that subtle something, that quality
+               of air that emanation from old trees, that so wonderfully changes
+               and renews a weary spirit.
+             </Typography>
+             <div className="flex gap-2">
+               <Button size="lg" color="white">
+                 Explore
+               </Button>
+               <Button size="lg" color="white" variant="text">
+                 Gallery
+               </Button>
+             </div>
+           </div>
+         </div>
+       </div>
+       <div className="relative h-full w-full">
+       <div className="h-full w-full overlay" >
+       <img
+        src={thirdSlide} alt="slide3"
+        className="h-full w-full object-cover"
+      />
+      </div>
+         <div className="absolute inset-0 grid h-full w-full items-end bg-black/75">
+           <div className="w-3/4 pl-12 pb-12 md:w-2/4 md:pl-20 md:pb-20 lg:pl-32 lg:pb-32">
+             <Typography
+               variant="h1"
+               color="white"
+               className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+             >
+               The Beauty of Nature
+             </Typography>
+             <Typography
+               variant="lead"
+               color="white"
+               className="mb-12 opacity-80"
+             >
+               It is not so much for its beauty that the forest makes a claim
+               upon men&apos;s hearts, as for that subtle something, that quality
+               of air that emanation from old trees, that so wonderfully changes
+               and renews a weary spirit.
+             </Typography>
+             <div className="flex gap-2">
+               <Button size="lg" color="white">
+                 Explore
+               </Button>
+               <Button size="lg" color="white" variant="text">
+                 Gallery
+               </Button>
+             </div>
+           </div>
+         </div>
+       </div>
   {/* Repeat the above structure for other slides */}
 </Carousel>
    
