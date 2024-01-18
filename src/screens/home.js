c126e4c9929cd122747188eef6dbca87124a6710
Given Fyneface <fynefaceg@gmail.com>
2024-01-18 15:56:52 +0100

    resposive

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import TradingWidget from '../widgets/trading-widget'
-import { Carousel, Typography, Button } from "@material-tailwind/react";
+import { Carousel, Typography, Button, IconButton } from "@material-tailwind/react";
 import firstSlide from '../assets/carousel1.jpg'
 import secondSlide from '../assets/carousel2.jpg'
 import thirdSlide from '../assets/carousel3.jpg'
@@ -17,16 +17,65 @@ const Home = () => {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <div>
+    <div  >
  
  
-     
-     <Carousel className="rounded-none" autoplay="true" autoplayDelay="5000" loop="true" transition={{ duration: 1 }}>
+     <div style={{height:"100vh"}}> 
+     <Carousel className="rounded-none h-full" autoplay="true" autoplayDelay="5000" loop="true" transition={{ duration: 1 }}
+      prevArrow={({ handlePrev }) => (
+        <IconButton
+        variant="filled"
+        color="gray"
+          size="lg"
+          onClick={handlePrev}
+          className="!absolute top-2/4 left-4 -translate-y-2/4"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="#94A3B8"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+            />
+          </svg>
+        </IconButton>
+      )}
+      nextArrow={({ handleNext }) => (
+        <IconButton
+          variant="filled"
+          color="gray"
+          size="lg"
+          onClick={handleNext}
+          className="!absolute top-2/4 right-4 -translate-y-2/4"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+            />
+          </svg>
+        </IconButton>
+      )}
+      >
   <div className="relative h-full w-full">
     <div className="h-full w-full overlay" >
     <img
       src={firstSlide} alt="office"
-      className="h-full w-full object-cover"
+      className="h-full w-full object-fit"
     />
     </div>
     <div className="absolute inset-0 grid h-full w-full place-items-center">
@@ -130,9 +179,9 @@ const Home = () => {
            </div>
          </div>
        </div>
-  {/* Repeat the above structure for other slides */}
+ 
 </Carousel>
-   
+   </div>
 
     <div className="bg-white">
       <header className="absolute inset-x-0 top-0 z-50">
